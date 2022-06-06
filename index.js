e920ea23b4fa2f59adc14da9e80a083addcc5760
Brian Wangila <bwanjalah@gmail.com>
2022-06-06 23:22:30 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+
+let num1;
+let num2;
+let multiply = 64;
+multiply = num1 * num2;
+
+
+ let random;
+ random = (Math.floor(Math.random() *10) + 1);
+ console.log(random)
+
+
+let mod = 4;
+let num3;
+let num4;
+mod = (num3 % num4)
+
+
+let max = (2, 54, 12, 7, 20);
+console.log(Math.max(max))
